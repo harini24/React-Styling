df392c1b00f231a1fd6b7455828ffa4ef47a11cd
harini <harinisiva1997@gmail.com>
2021-05-15 11:39:07 +0530

    css modules synamic styles and med quer

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -4,43 +4,43 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 import styled from 'styled-components'
 
-const FormControl = styled.div`
+// const FormControl = styled.div`
 
-  margin: 0.5rem 0;
+  // margin: 0.5rem 0;
 
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props=>(props.invalid ? '#ffd7d7' : 'black')};
-}
+// & label {
+  // font-weight: bold;
+  // display: block;
+  // margin-bottom: 0.5rem;
+  // color: ${props=>(props.invalid ? '#ffd7d7' : 'black')};
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props=>(props.invalid ? 'red' : '#ccc')};
-  background : ${props=>(props.invalid ? '#ffd7d7' : 'transparent')};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+  // display: block;
+  // width: 100%;
+  // border: 1px solid ${props=>(props.invalid ? 'red' : '#ccc')};
+  // background : ${props=>(props.invalid ? '#ffd7d7' : 'transparent')};
+  // font: inherit;
+  // line-height: 1.5rem;
+  // padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+// & input:focus {
+  // outline: none;
+  // background: #fad0ec;
+  // border-color: #8b005d;
+// }
 
-&.invalid input{
-  border-color: red;
-  background: rgb(199, 155, 155);
-}
+// &.invalid input{
+  // border-color: red;
+  // background: rgb(199, 155, 155);
+// }
 
-&.invalid label{
-  color: red;
-}
-`
+// &.invalid label{
+  // color: red;
+// }
+// `
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid,setIsvalid] = useState(true)
@@ -63,10 +63,10 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* <FormControl className={!isValid && 'invalid'}> */}
-      <FormControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
